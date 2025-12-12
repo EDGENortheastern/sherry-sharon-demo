931bcdc7089b3021c3e357073c0946daafaf5982
Katia <katia.punter@storyshareplatform.com>
2025-12-12 12:50:41 +0000

    name

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Name App</h1>
+      <h2>UAT env</h2>
       <label htmlFor="name">Name</label>
       <input type="text" id="name" />
     </div>
